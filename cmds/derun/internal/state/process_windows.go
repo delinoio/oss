e33a4d656b045815cf9832a321352b4dff78c330
DongYun Kang <kdy.1997.dev@gmail.com>
2026-02-24 18:01:32 +0900

    ci: fix cross-platform test and typecheck regressions

--- cmds/derun/internal/state/process_windows.go
+++ cmds/derun/internal/state/process_windows.go
@@ -2,6 +2,22 @@
 
 package state
 
+import "golang.org/x/sys/windows"
+
 func processAlive(pid int) bool {
-	return false
+	if pid <= 0 {
+		return false
+	}
+
+	processHandle, err := windows.OpenProcess(windows.SYNCHRONIZE, false, uint32(pid))
+	if err != nil {
+		return false
+	}
+	defer windows.CloseHandle(processHandle)
+
+	waitStatus, err := windows.WaitForSingleObject(processHandle, 0)
+	if err != nil {
+		return false
+	}
+	return waitStatus == windows.WAIT_TIMEOUT
 }
